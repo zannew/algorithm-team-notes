26eab44e1d81534a124aa0653d026cc1514595cc
“zannew“ <wonny21292@gmail.com>
2021-08-10 14:57:12 +0900

    added how to remove the all of matching elements

--- basic/list_methods.py
+++ basic/list_methods.py
@@ -21,9 +21,19 @@ print("reverse : ", a)
 a.insert(2, 3)
 print("added 3 at 2 : ", a)
 
+a. insert(len(a), 7)
+print("added 7 at the end: ", a)
+
 # counting
 print("number of data which values of 3 : ", a.count(3))
 
 # removes the first matching element
 a.remove(3)
 print("removed : ", a)
+
+# removes the all of matching elements
+remove_set = [3, 7]
+
+# Result has values not in "remove_set"
+result = [i for i in a if i not in remove_set]
+print("result : ", result)
