897fd9e7a35f508503ec610a20a3ba948178cfa9
“zannew“ <wonny21292@gmail.com>
2021-08-17 22:58:13 +0900

    stack and queue

---
+++ DFSandBFS/queue_ex.py
@@ -0,0 +1,20 @@
+'''
+큐 자료구조 구현 (First In First Out)
+'''
+# 시간 복잡도가 더 높고 비효율적으로 동작할 수 있으므로 deque 라이브러리 사용 권장
+from collections import deque
+
+queue = deque()
+
+queue.append(5)
+queue.append(2)
+queue.append(3)
+queue.append(7)
+queue.popleft()
+queue.append(1)
+queue.append(4)
+queue.popleft()
+
+print(queue) # 먼저 들어온 순서대로 출력 (queue)
+queue.reverse() # 뒤집기
+print(queue) # 나중에 들어온 원소부터 출력
---
+++ DFSandBFS/stack_ex.py
@@ -0,0 +1,17 @@
+'''
+스택 자료구조 구현 (Last In First Out)
+'''
+
+list = []
+
+list.append(5)
+list.append(2)
+list.append(3)
+list.append(7)
+list.pop()
+list.append(1)
+list.append(4)
+list.pop()
+
+print(list[::-1])
+print(list)
