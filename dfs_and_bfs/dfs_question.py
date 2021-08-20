bae07d881cec0b10777583c7b3e088951205dbcc
“zannew“ <wonny21292@gmail.com>
2021-08-20 18:35:10 +0900

    edited

--- dfs_and_bfs/dfs_question.py
+++ dfs_and_bfs/dfs_question.py
@@ -32,4 +32,3 @@ for i in range(n):
             result += 1
 
 print(result)
-
---
+++ test_here.py
@@ -0,0 +1,19 @@
+'''greedy question'''
+reserve = [1]
+lost = [3]
+n = 3
+
+participants = []
+
+def solution(n, lost, reserve):
+    set_reserve = set(reserve) - set(lost)
+    set_lost = set(lost) - set(reserve)
+    for i in set_reserve:
+        if i-1 in set_lost:
+            set_lost.remove(i-1)
+        elif i+1 in set_lost:
+            set_lost.remove(i+1)
+    print(n - len(set_lost))
+    return n - len(set_lost)
+
+solution(n, lost, reserve)
